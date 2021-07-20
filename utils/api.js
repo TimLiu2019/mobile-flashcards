bc733e0dc4aa14e26923e66c84e9232adcefcc46
Tim <qitingliu7@gmail.com>
2021-07-20 10:17:52 -0400

    uitls

---
+++ utils/api.js
@@ -0,0 +1,27 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// return all of the decks along with their titles, 
+// questions, and answers
+export function getDecks(){
+
+}
+
+// take in a single id argument 
+//and return the deck associated with that id. 
+export function getDeck(){
+
+}
+
+//take in a single title argument and add it to the decks. 
+export function saveDeckTitle(){
+
+}
+
+//  take in two arguments, title and card, and will 
+//add the card to the list of questions 
+//for the deck with the associated title. 
+
+export function addCardToDeck(){
+
+}
+

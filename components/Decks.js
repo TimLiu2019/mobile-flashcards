5698a4d863e5c2a1fdb2b24a4369bcecdc6052ce
Tim <qitingliu7@gmail.com>
2021-07-27 21:03:11 -0400

    Decks: get decks string from AsyncStorage

--- components/Decks.js
+++ components/Decks.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
 const Decks = () => {
+  const [decks, setDecks] = useState('');
+  useEffect(async() => {
+   
+      try {
+        const valueString = await AsyncStorage.getItem("DECKS");
+        console.log('received deck string',valueString);
+        const value = JSON.parse(valueString);
+        // Other set states
+        setDecks(valueString);
+      //  console.log(value);
+      } catch (error) {
+        console.log(error);
+      }
     
+  },[]);
   return (
     <SafeAreaView>
       <View>
-        <Text>Decks</Text>
+        <Text>{decks}</Text>
       </View>
     </SafeAreaView>
   );
